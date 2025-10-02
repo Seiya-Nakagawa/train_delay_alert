54914db8125e830331f44361198d2ee117206c3d
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-10-02 23:09:22 +0900

    Lambda用ロググループ名が循環するため、lambdaリソース参照を取り消し

--- terraform/cloudwatch_logs.tf
+++ terraform/cloudwatch_logs.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "user_settings_lambda_lg" {
-  name              = "/aws/lambda/${aws_lambda_function.user_settings_lambda.function_name}"
+  name              = "/aws/lambda/${local.name_prefix}-lambda-user-settings"
   retention_in_days = 1
 
   tags = merge(local.tags, {
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_log_group" "user_settings_lambda_lg" {
 }
 
 resource "aws_cloudwatch_log_group" "check_delay_lambda_lg" {
-  name              = "/aws/lambda/${aws_lambda_function.check_delay_lambda.function_name}"
+  name              = "/aws/lambda/${local.name_prefix}-lambda-check-delay"
   retention_in_days = 1
 
   tags = merge(local.tags, {
