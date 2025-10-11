487c024ab2400d68b75edd0a9bbafe7549beed37
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-10-11 13:54:21 +0900

    パラメータストアのvalueをTerraform管理から除外

--- terraform/systems_manager.tf
+++ terraform/systems_manager.tf
@@ -1,7 +1,13 @@
 resource "aws_ssm_parameter" "line_channel_secret" {
   name  = "/${local.name_prefix}/line/channel_secret"
   type  = "SecureString"
-  value = "dummy-secret"
+  value = "dummy-secret-value-to-be-changed-in-console"
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 
   tags = merge(local.tags, {
     Name = "/${local.name_prefix}/line/channel_secret"
@@ -11,7 +17,13 @@ resource "aws_ssm_parameter" "line_channel_secret" {
 resource "aws_ssm_parameter" "line_channel_access_token" {
   name  = "/${local.name_prefix}/line/channel_access_token"
   type  = "SecureString"
-  value = "dummy-access-token"
+  value = "dummy-access-token-to-be-changed-in-console"
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 
   tags = merge(local.tags, {
     Name = "/${local.name_prefix}/line/channel_access_token"
@@ -21,7 +33,13 @@ resource "aws_ssm_parameter" "line_channel_access_token" {
 resource "aws_ssm_parameter" "traffic_api_token" {
   name  = "/${local.name_prefix}/traffic/api_token"
   type  = "SecureString"
-  value = "dummy-api-token"
+  value = "dummy-api-token-to-be-changed-in-console"
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 
   tags = merge(local.tags, {
     Name = "/${local.name_prefix}/traffic/api_token"
