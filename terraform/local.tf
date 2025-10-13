1592740b1b106baf9ab7a0199d0567bf5cf2fe36
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-10-13 22:05:18 +0900

    Lambdaライブラリ配置のためS3バケット作成

--- terraform/local.tf
+++ terraform/local.tf
@@ -1,6 +1,11 @@
 locals {
   name_prefix = "${var.system_name}-${var.env}"
 
+  # ここに作成したいフォルダ名を追加・削除するだけでOK
+  s3_folder_names = toset([
+    "lambda-layers/"
+  ])
+
   tags = {
     SystemName = var.system_name,
     Env        = var.env
---
+++ terraform/s3.tf
@@ -0,0 +1,70 @@
+resource "aws_s3_bucket" "s3_train_alert" {
+  bucket = "${var.system_name}-${var.env}-s3"
+
+  tags = {
+    Name       = "${var.system_name}-${var.env}-s3",
+    SystemName = var.system_name,
+    Env        = var.env,
+  }
+}
+
+## 暗号化
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption_train_alert" {
+  bucket = aws_s3_bucket.s3_train_alert.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+## バージョニング設定
+resource "aws_s3_bucket_versioning" "versioning_train_alert_results" {
+  bucket = aws_s3_bucket.s3_train_alert.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "folders" {
+  # for_eachに、上で定義したフォルダ名のセットを渡します
+  for_each = local.s3_folder_names
+
+  # フォルダを作成したいバケットのIDを指定
+  bucket = aws_s3_bucket.s3_train_alert.id
+
+  # each.keyには、"gas_url/"、"processed_files/"などのフォルダ名が順番に入ります
+  key = each.key
+
+  # フォルダであることを示すContent-Type
+  content_type = "application/x-directory"
+
+  # 中身は空
+  content = ""
+
+  # 空のコンテンツのMD5ハッシュ値を指定
+  etag = md5("")
+}
+
+
+# ライフサイクルルール (最新3世代を保持)
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_train_alert" {
+  # ライフサイクルルールはバージョニングが有効になっている必要があるため、depends_onを追加します
+  depends_on = [aws_s3_bucket_versioning.versioning_train_alert_results]
+
+  bucket = aws_s3_bucket.s3_train_alert.id
+
+  rule {
+    id     = "keep-last-3-versions"
+    status = "Enabled"
+
+    # バケット内のすべてのオブジェクトにルールを適用
+    filter {}
+
+    # 非現行（古い）バージョンのオブジェクトに対するアクション
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+      newer_noncurrent_versions = 2
+    }
+  }
+}
