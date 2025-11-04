007f2ca6b34053d0729121c62d7a68c6752ce601
Seiya-Nakagagwa <aibdlnew1.work@gmail.com>
2025-11-04 21:29:15 +0900

    デプロイテスト

--- terraform/lambda/check_delay_handler.py
+++ terraform/lambda/check_delay_handler.py
@@ -569,3 +569,5 @@ def lambda_handler(event, context):
         # ハンドラ全体で予期せぬエラーをキャッチし、ログに出力
         logger.critical("lambda_handlerで予期せぬエラーが発生しました", exc_info=True)
         return {"statusCode": 500, "body": json.dumps(str(e), ensure_ascii=False)}
+        print(e)
+        raise e
